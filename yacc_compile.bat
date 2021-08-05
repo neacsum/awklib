ff66ccda0a349fc829c4e5bda4b0d26e769e7af1
Mircea Neacsu <mircea@neacsu.net>
2021-08-05 11:03:13 -0400

    Added batch file to run YACC compiler

---
+++ yacc_compile.bat
@@ -0,0 +1,7 @@
+@echo off
+del -y ytab.h
+yacc -d -o ytab.cpp awkgram.y
+REM Different versions of YACC give different naems to .H file
+REM It can be y.tab.h or ytab.h
+if exist ytab.h goto :EOF
+ren ytab.tab.h ytab.h
